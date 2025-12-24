d72f91cb7ffa82f229837a6795a2433c04a419cd
Theikdi Sann <theikdisann2001@gmail.com>
2025-12-25 00:31:42 +0700

    Fixed and added tables

--- project_m_0001.sql
+++ project_m_0001.sql
@@ -1,166 +1,5 @@
-# AUTHENTICATION
-
-user {
-    id      : VARCHAR(25)
-    username: VARCHAR(50) UNIQUE
-    password: VARCHAR(50)
-    fullname: VARCHAR(100)
-    email   : VARCHAR(100) UNIQUE
-    phone   : VARCHAR(15)
-    role    : Role (DEFAULT Waiter)
-    isActive: BOOLEAN (DEFAULT TRUE)
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    
-}
-
-user_session {
-    id            : VARCHAR(25)
-    user_id       : VARCHAR(50)
-    session_token : VARCHAR(255)
-    ip_address    : INET
-    user_agent    : VARCHAR(255)
-    expires_at    : TIMESTAMP
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-}
-
-role {
-    id:        : VARCHAR(25)
-    name       : VARCHAR(30)       
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-}
-
-permission {
-    id: 
-    role_id: 
-    name: VARCHAR(25)
-    can_create : bool
-    can_read : bool
-    can_update : bool
-    can_delete : bool
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-}
-
-# ADMINISTRATION
-
-# MENU MANAGEMENT
-
-menu_category {
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(50) NOT NULL,
-    description TEXT,
-    status : ENUM('active', 'inactive'),
-    
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-}
-
-menu_item {
-    id : VARCHAR(25),
-    name : VARCHAR(255) NOT NULL,
-    description : VARCHAR(255),
-    category_id : menu_categories(id),
-    
-    -- Operational
-    preparation_time INT, -- in minutes
-    status VARCHAR(10) DEFAULT 'available' CHECK (status IN ('available', 'unavailable')),
-    ingredients TEXT,
-    chef_notes TEXT,
-    image_url VARCHAR(255),
-
-    
-    -- Options
-    option
-    
-    -- Timestamps
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-    -- Indexes
-    INDEX idx_menu_items_category (category_id),
-    INDEX idx_menu_items_status (status),
-    INDEX idx_menu_items_buffet (available_for_buffet)
-}
-
-options {
-    id: 
-    name: 
-    
-}
-
-menu_item_session_details {
-    id : 
-    session_type_id : 
-    menu_item_id : 
-    price : 
-    pricing_model : ENUM ("INCLUDED", "EXTRA_CHARGE")
-}
-
-# ORDER MANAGEMENT
-
-# SESSION & BILLING
-
-session (
-    id : VARCHAR(25)
-    token: VARCHAR(255) UNIQUE
-    table_id : Table(id),
-    session_type : SessionTypes(id)
-    
-    customer_count : INTEGER,
-    
-    -- Timing
-    start_time:  TIMESTAMP NOT NULL,
-    end_time : TIMESTAMP,
-    expires_at : TIMESTAMP,
-    
-    -- Status
-    status : ENUM('active', 'bill_requested', "awaiting_payment", 'ended', 'paid', 'cancelled'),
-    
-    -- Staff assignment
-    created_by UUID NOT NULL REFERENCES users(id),
-    
-    -- Customer information
-    customer_notes TEXT,
-    
-    -- Timestamps
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-    -- Indexes
-    INDEX idx_sessions_table (table_id),
-    INDEX idx_sessions_status (status),
-    INDEX idx_sessions_type (session_type),
-    INDEX idx_sessions_timing (start_time, end_time),
-);
-
-session_type {
-    id           : VARCHAR(25)
-    name         : VARCHAR(255)
-    display_name : VARCAHR(255)
-}
-
-payment {
-    id                : VARCHAR(25),
-    invoice_id        : Invoices(id),
-    payment_method_id : payment_method(id),
-    amount            : DECIMAL,
-    transaction_id    : VARCHAR(100),
-    status            : ENUM('pending', 'completed', 'failed', 'refunded'),
-}
-
-payment_method {
-    id   : VARCHAR(25)
-    name : VARCHAR(50)
-}
-
-
-# MYSQL
-
 -- AUTHENTICATION & ADMINISTRATION
+
 CREATE TABLE roles (
     id VARCHAR(25) PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL,
@@ -306,7 +145,7 @@ CREATE TABLE option_values (
 	FOREIGN KEY (option_id) REFERENCES options(id)
 );
 
-CREATE TABLE option_value_session_pricing (
+CREATE TABLE session_type_option_value_additional_prices (
 	id VARCHAR(25) PRIMARY KEY,
 	additional_price DECIMAL(10,2) NOT NULL DEFAULT 0,
 	session_type_id VARCHAR(25) NOT NULL,
@@ -362,6 +201,7 @@ CREATE TABLE menu_item_session_details (
 	FOREIGN KEY (pricing_model_id) REFERENCES pricing_models(id)
 );
 
+
 -- ORDER MANAGEMENT
 
 CREATE TABLE orders (
@@ -373,12 +213,12 @@ CREATE TABLE orders (
 
     status VARCHAR(50) NOT NULL DEFAULT 'Pending', -- e.g., Pending, Preparing, Ready, Served, Cancelled
 
-    special_instructions TEXT
+    special_instructions TEXT,
     
     FOREIGN KEY (session_id) REFERENCES sessions(id),
     FOREIGN KEY (created_by) REFERENCES users(id),
     FOREIGN KEY (updated_by) REFERENCES users(id),
-    FOREIGN KEY (served_by) REFERENCES users(id),
+    FOREIGN KEY (served_by) REFERENCES users(id)
 );
 
 CREATE TABLE order_items (
@@ -408,6 +248,8 @@ CREATE TABLE order_item_selected_options (
     FOREIGN KEY (order_item_id) REFERENCES order_items(id),
     FOREIGN KEY (option_value_id) REFERENCES option_values(id)
 );
+
+-- Invoice & Billing
 
 CREATE TABLE invoices (
     id VARCHAR(25) PRIMARY KEY,
@@ -450,7 +292,7 @@ CREATE TABLE invoice_adjustments (
     type VARCHAR (20) NOT NULL CHECK (type IN ('FIXED', 'PERCENTAGE')),
 
     value DECIMAL(15, 2) DEFAULT 0.00, -- Stores either % or $ amount
-
+    
     is_enabled BOOLEAN DEFAULT TRUE,
 
     created_by VARCHAR(25) NOT NULL,
@@ -484,6 +326,3 @@ CREATE TABLE payments (
     
     INDEX idx_payments_invoice (invoice_id)
 );
-
-    SHOW TABLES;
-    SELECT * FROM permissions;
