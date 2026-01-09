6cc43133c79fef3ff4eb280cd2c785b6ca822907
TheikdiSann <theikdisann2001@gmail.com>
2026-01-09 10:59:45 +0700

    feat: integrate next-auth for authentication and add login functionality

---
+++ website/src/components/LoginButton.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { login } from "@/lib/actions/auth";
+
+export default function LoginButton() {
+  return (
+    <div>
+        <button className="bg-blue-500, text-white, p-2, rounded" onClick={() => login()}>Sign In with Google</button>
+    </div>
+  )
+}
