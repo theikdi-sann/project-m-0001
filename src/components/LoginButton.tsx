c21f4d2c6d983d2327f7c1eb22c23f29c6fcf891
TheikdiSann <theikdisann2001@gmail.com>
2026-01-09 12:24:11 +0700

    style: format code for consistency and readability

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,16 @@
-'use client';
+"use client";
 
 import { login } from "@/lib/actions/auth";
 
 export default function LoginButton() {
   return (
     <div>
-        <button className="bg-blue-500, text-white, p-2, rounded" onClick={() => login()}>Sign In with Google</button>
+      <button
+        className="bg-blue-500, text-white, p-2, rounded"
+        onClick={() => login()}
+      >
+        Sign In with Google
+      </button>
     </div>
-  )
+  );
 }
--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -2,10 +2,10 @@
 
 import { signIn, signOut } from "@/auth";
 
-export const login = async() => {
-    await signIn("google", {redirectTo: "/"})
-}
+export const login = async () => {
+  await signIn("google", { redirectTo: "/" });
+};
 
-export const logout = async() => {
-    await signOut({redirectTo: "/"})
-}
+export const logout = async () => {
+  await signOut({ redirectTo: "/" });
+};
