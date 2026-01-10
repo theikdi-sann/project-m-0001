28eb5dd029b7b21da27463205ecf2d807fc0f1aa
TheikdiSann <theikdisann2001@gmail.com>
2026-01-11 01:53:05 +0700

    feat(auth): enhance NextAuth integration with user role management

--- website/src/auth.ts
+++ website/src/auth.ts
@@ -2,20 +2,26 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { prisma } from "../lib/prisma";
+import { UserRole } from "./generated/prisma/enums";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
   callbacks: {
-    async signIn({account, profile}){
+    async signIn({ account, profile }) {
+      // Google Check
+      if (account?.provider === "google") {
+        return !!profile?.email_verified;
+      }
+      // Other provider Check
 
-        // Google Check
-        if(account?.provider === "google") {
-            return !!profile?.email_verified}
-
-        // Other provider Check
-        
-        return false
-    }
+      return false;
+    },
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.role = user.role as UserRole;
+      }
+      return session;
+    },
   },
 });
